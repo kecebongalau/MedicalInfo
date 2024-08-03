13817273931b79866ac3375e53e0b202c07e14e3
Emilio Garvin <116335067+kecebongalau@users.noreply.github.com>
2024-08-03 23:03:01 +0800

    Update SQLQuery4 - Audit.sql

--- SQLQuery4 - Audit.sql
+++ SQLQuery4 - Audit.sql
@@ -1,7 +1,9 @@
+-- creating DDL and DCL Activities Audit
 CREATE SERVER AUDIT DDLActivities_Audit TO FILE ( FILEPATH = 'C:\kuliah\year 3\logs');
 
 ALTER SERVER AUDIT DDLActivities_Audit WITH (STATE = ON);
 
+-- adding the DDL and DCL Specification
 CREATE SERVER AUDIT SPECIFICATION [DDLActivities_Audit_Specification ]
 FOR SERVER AUDIT [DDLActivities_Audit]
 ADD (DATABASE_OBJECT_CHANGE_GROUP), 
@@ -13,17 +15,22 @@ ADD (SERVER_OBJECT_PERMISSION_CHANGE_GROUP),
 ADD (DATABASE_PERMISSION_CHANGE_GROUP)
 WITH (STATE=ON);
 
+-- creating DML Activities Audit
 CREATE SERVER AUDIT DMLActivities_Audit TO FILE ( FILEPATH = 'C:\kuliah\year 3\logs' );
 
 ALTER SERVER AUDIT DMLActivities_Audit WITH (STATE = ON);
 USE MedicalInfoSystem;
 GO;
+
+-- Creating DML Specification
 CREATE DATABASE AUDIT SPECIFICATION DMLActivities_Audit_Spec
 FOR SERVER AUDIT DMLActivities_Audit  
 ADD ( INSERT , UPDATE, DELETE, SELECT
 ON DATABASE::[MedicalInfoSystem] BY public)   
 WITH (STATE = ON) ;
 
+
+-- Showing the DDL and DCL Activities
 DECLARE @AuditFilePath VARCHAR(8000);
 Select @AuditFilePath = audit_file_path
 From sys.dm_server_audit_status
@@ -39,6 +46,8 @@ inner join sys.dm_audit_actions b
 on a.action_id = b.action_id
 order by a.event_time desc
 
+
+-- Showing the DML Activities
 DECLARE @AuditFilePath VARCHAR(8000);
 Select @AuditFilePath = audit_file_path
 From sys.dm_server_audit_status
@@ -53,8 +62,8 @@ inner join sys.dm_audit_actions b
 on a.action_id = b.action_id
 order by a.event_time desc
 
+-- saving the log activities in file
 CREATE SERVER AUDIT LogingLogoutActivities_Audit TO FILE ( FILEPATH = 'C:\kuliah\year 3\logs' );
-
 ALTER SERVER AUDIT LogingLogoutActivities_Audit WITH (STATE = ON);
 
 CREATE SERVER AUDIT SPECIFICATION [LogingLogoutActivities_Audit_Specification ]
@@ -65,6 +74,12 @@ ADD (DATABASE_LOGOUT_GROUP),
 ADD (LOGOUT_GROUP)
 WITH (STATE=ON);
 
+-- saving the log activies in application log
+ALTER SERVER AUDIT LogingLogoutActivities_Audit 
+TO APPLICATION_LOG
+WITH (QUEUE_DELAY = 1000, ON_FAILURE = CONTINUE);
+
+-- Showing the log activies
 DECLARE @AuditFilePath VARCHAR(8000);
 Select @AuditFilePath = audit_file_path
 From sys.dm_server_audit_status
@@ -79,60 +94,5 @@ inner join sys.dm_audit_actions b
 on a.action_id = b.action_id
 order by a.event_time desc
 
-USE MedicalInfoSystem;
 
-CREATE TABLE DDLAudit (
-    EventID INT IDENTITY(1,1) PRIMARY KEY,
-    ActionType NVARCHAR(50),
-    ActionTime DATETIME DEFAULT GETDATE(),
-    Username NVARCHAR(128),
-    EventData XML
-);
 
-CREATE TABLE DCLAudit (
-    EventID INT IDENTITY(1,1) PRIMARY KEY,
-    ActionType NVARCHAR(50),
-    ActionTime DATETIME DEFAULT GETDATE(),
-    Username NVARCHAR(128),
-    EventData XML
-);
-
-USE MedicalInfoSystem;
-
-ALTER TRIGGER trgDDLAudit
-ON DATABASE
-FOR CREATE_TABLE, ALTER_TABLE, DROP_TABLE, CREATE_PROCEDURE, ALTER_PROCEDURE, DROP_PROCEDURE, CREATE_TRIGGER, ALTER_TRIGGER, DROP_TRIGGER, CREATE_VIEW, ALTER_VIEW, DROP_VIEW
-AS
-BEGIN
-    -- Insert into audit table for DDL actions only if in the specific database
-    IF DB_NAME() = 'MedicalInfoSystem'
-    BEGIN
-        INSERT INTO DDLAudit (ActionType, ActionTime, Username, EventData)
-        SELECT
-            EVENTDATA().value('(/EVENT_INSTANCE/EventType)[1]', 'NVARCHAR(50)') AS ActionType,
-            GETDATE() AS ActionTime,
-            USER_NAME() AS Username,
-            EVENTDATA() AS EventData;
-    END
-END;
-
-USE MedicalInfoSystem;
-CREATE TRIGGER trgDCLAudit
-ON DATABASE
-FOR GRANT_DATABASE, REVOKE_DATABASE, DENY_DATABASE
-AS
-BEGIN
-    -- Insert into audit table for DCL actions only if in the specific database
-    IF DB_NAME() = 'MedicalInfoSystem'
-    BEGIN
-        INSERT INTO DCLAudit (ActionType, ActionTime, Username, EventData)
-        SELECT
-            EVENTDATA().value('(/EVENT_INSTANCE/EventType)[1]', 'NVARCHAR(50)') AS ActionType,
-            GETDATE() AS ActionTime,
-            USER_NAME() AS Username,
-            EVENTDATA() AS EventData;
-    END
-END;
-
-select * from DCLAudit
-select * from DDLAudit
