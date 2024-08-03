d084b4eadf7558e973071470b774693d37e34c7c
Emilio Garvin <116335067+kecebongalau@users.noreply.github.com>
2024-08-03 23:02:49 +0800

    Update SQLQuery3 - Create data admin.sql

--- SQLQuery3 - Create data admin.sql
+++ SQLQuery3 - Create data admin.sql
@@ -20,7 +20,10 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON Patient TO Data_Admin;
 DENY SELECT,  UPDATE ON Patient(PPhone, PaymentCardNo) TO Data_Admin
 GRANT SELECT, INSERT, UPDATE, DELETE ON Doctor TO Data_Admin;
 DENY SELECT, UPDATE ON Doctor(DPhone, DPass) TO Data_Admin
+--GRANT SELECT ON Doctor(DPhone) TO Data_Admin
+--only used to demo Dynamic Data Masking
 GRANT INSERT ON DCLAudit To Data_Admin;
+DENY UNMASK TO Data_Admin
 GO
 ALTER SERVER ROLE securityadmin ADD MEMBER DA001;
 
