300e420430eccd41dc7795809234580fa3aba77e
Emilio Garvin <116335067+kecebongalau@users.noreply.github.com>
2024-08-03 23:03:12 +0800

    Update StoredProcedure - Create Role and Grant Permission.sql

--- StoredProcedure - Create Role and Grant Permission.sql
+++ StoredProcedure - Create Role and Grant Permission.sql
@@ -1,19 +1,1 @@
 
-CREATE ROLE Doctor;
--- ROLE PERMISSION
-
-GRANT SELECT, UPDATE ON Doctor TO Doctor;
-GRANT SELECT, INSERT, UPDATE ON Diagnosis TO Doctor;
-GRANT SELECT ON Patient TO Doctor;
-
-
-CREATE ROLE Patients;
--- ROLE PERMISSION
-
-GRANT SELECT ON Diagnosis TO Patients;
-GRANT SELECT, UPDATE ON Patient TO Patients;
-
-
-
-
-
