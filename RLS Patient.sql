4d151ab517577190581ee22b0f6ddaa23adf1567
Emilio Garvin <116335067+kecebongalau@users.noreply.github.com>
2024-08-03 23:01:31 +0800

    Update RLS Patient.sql

--- RLS Patient.sql
+++ RLS Patient.sql
@@ -3,7 +3,7 @@
 CREATE SCHEMA Security;  
 GO  
 
-CCREATE FUNCTION Security.fn_securitypredicate
+CREATE FUNCTION Security.fn_securitypredicate
 	(@UserName AS nvarchar(100))  
 RETURNS TABLE  
 WITH SCHEMABINDING  
@@ -15,53 +15,77 @@ GO
 CREATE SECURITY POLICY [MIS_SecurityPolicy]   
 ADD FILTER PREDICATE 
 [Security].[fn_securitypredicate](PID) 
-ON [dbo].[Patient]
+ON [dbo].[Patient] 
 
--- RLS using view
-Create Procedure GetCustomerData
-AS
-BEGIN
-Select *
-From Doctor
-where DrID = USER_NAME()
-END
 
-CREATE PROCEDURE [dbo].[SP_UpdatePatientDetails] 
+ALTER PROCEDURE [dbo].[SP_UpdatePatientDetails] 
     @Pname VARCHAR(100) = NULL,
     @PPhone VARCHAR(50) = NULL,
     @PaymentCardNo VARCHAR(50) = NULL
 AS
 BEGIN
+    -- Declare necessary variables
+    DECLARE @PID VARCHAR(50);
+    DECLARE @CurrentEncryptedPPhone VARBINARY(MAX);
+    DECLARE @CurrentEncryptedPaymentCardNo VARBINARY(MAX);
 
-    OPEN SYMMETRIC KEY SimKey1
+    -- Open the symmetric key for encryption/decryption
+    OPEN SYMMETRIC KEY  Simkey1
     DECRYPTION BY CERTIFICATE CertForCLE;
-    DECLARE @PID VARCHAR(50)
-    DECLARE @DecryptedPPhone VARCHAR(50)
-    DECLARE @DecryptedPaymentcardno VARCHAR(50)
 
-	SELECT @PID = PID FROM [Patient]
-	-- Checking
-    IF @PID != USER_NAME()
+    -- Fetch the PID based on the logged-in user
+    SELECT @PID = PID 
+    FROM [Patient]
+    WHERE PID = USER_NAME();
+
+    -- Check if the user is authorized to update their own details
+    IF @PID IS NULL
     BEGIN 
-        PRINT 'You are not authorised to perform this transaction' 
-        RETURN 
+        PRINT 'You are not authorized to perform this transaction'; 
+        CLOSE SYMMETRIC KEY SymKey1;
+        RETURN;
     END 
-    -- Decrypt the phone number and payment card number to save original decrypted values
 
-    SET @DecryptedPPhone = CONVERT(VARCHAR, DECRYPTBYKEY(@PPhone));
-    SET @DecryptedPaymentcardno = CONVERT(VARCHAR, DECRYPTBYASYMKEY(ASYMKEY_ID('MyAsymKey'), @PaymentCardNo));
+    -- Fetch current encrypted values
+    SELECT 
+        @CurrentEncryptedPPhone = PPhone,
+        @CurrentEncryptedPaymentCardNo = PaymentCardNo
+    FROM Patient
+    WHERE PID = @PID;
+
+    -- Decrypt the current values if needed
+    DECLARE @DecryptedPPhone VARCHAR(50) = CONVERT(VARCHAR(50), DECRYPTBYKEY(@CurrentEncryptedPPhone));
+    DECLARE @DecryptedPaymentCardNo VARCHAR(50) = CONVERT(VARCHAR(50), DECRYPTBYASYMKEY(ASYMKEY_ID('MyAsymKey'), @CurrentEncryptedPaymentCardNo));
 
     -- Update the patient's details
     UPDATE Patient
     SET 
         Pname = COALESCE(@Pname, Pname),
-        PPhone = COALESCE(ENCRYPTBYKEY(KEY_GUID('MyAsymKey'), @DecryptedPPhone), PPhone),
-        Paymentcardno = COALESCE(ENCRYPTBYASYMKEY(ASYMKEY_ID('PatAsymKey'), @DecryptedPaymentcardno), Paymentcardno)
+        PPhone = COALESCE(ENCRYPTBYKEY(KEY_GUID('SimKey1'), @PPhone), @CurrentEncryptedPPhone),
+        PaymentCardNo = COALESCE(ENCRYPTBYASYMKEY(ASYMKEY_ID('MyAsymKey'), @PaymentCardNo), @CurrentEncryptedPaymentCardNo)
     WHERE PID = @PID;
 
     -- Close the symmetric key
-    CLOSE SYMMETRIC KEY Simkey1;
+    CLOSE SYMMETRIC KEY  Simkey1;
 END;
 GO
 
+CREATE PROCEDURE View_Patient
+AS
+BEGIN
+    OPEN SYMMETRIC KEY SimKey1
+    DECRYPTION BY CERTIFICATE CertForCLE;
+    SELECT 
+        [PID], 
+        [Pname],
+        CONVERT(VARCHAR, DECRYPTBYKEY(PPhone)) AS DecryptedPphone, 
+        CONVERT(VARCHAR, DECRYPTBYASYMKEY(ASYMKEY_ID('MyAsymKey'), PaymentCardNo)) AS DecryptedPaymentcardno
+    FROM 
+        Patient;
+    CLOSE SYMMETRIC KEY SimKey1;
+END
+
+GRANT EXEC ON View_Patient TO Patients
 GRANT EXEC ON SP_UpdatePatientDetails TO Patients
+
+
